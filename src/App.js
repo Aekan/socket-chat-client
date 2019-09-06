bc3ea6df026d1aa7774ff9d194f42ee4f7b556f4
Aekan <alpe15.1992@gmail.com>
2019-09-06 16:40:54 +0200

    test #1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from "react";
+import socketIOClient from "socket.io-client";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            response: 0,
+            endpoint: "http://127.0.0.1:4001",
+            client: null
+        };
+
+        this.sendMessage = this.sendMessage.bind(this);
+    }
+
+    componentDidMount() {
+        const {endpoint} = this.state;
+        
+        this.socket = socketIOClient(endpoint);
+        
+        this.socket.on("outgoing data", data => this.setState({response: data.num}));
+        this.socket.on("chat message", data => {
+            const el = document.getElementById('messages');
+            const newMessageEl = document.createElement('li');
+            newMessageEl.textContent = data;
+            el.append(newMessageEl);
+        });
+    }
+
+    componentWillUnmount() {
+        this.socket.disconnect();
+        this.socket.close();
+    }
+
+    sendMessage(e) {
+        e.preventDefault();
+
+        const el = document.getElementById('m');
+        if (el.value) {
+            this.socket.emit('chat message', el.value);
+            el.value = "";
+        }
+    }
+
+    render() {
+        const {response} = this.state;
+        return (
+            <div style={{textAlign: "center"}}>
+                {response}
+                <ul id="messages"></ul>
+                <div className="form" action="">
+                    <input id="m" autoComplete="off" />
+                    <button onClick={this.sendMessage}>Send</button>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default App;
